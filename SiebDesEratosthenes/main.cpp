9c53fd7d0a0bab7cc21292e6c70bb8a2611f726d
cgerloff <cgerloff@fuchs.de>
2014-10-26 15:29:42 +0100

    Initial Commit

---
+++ SiebDesEratosthenes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SiebDesEratosthenes
+//
+//  Created by Gerloff, Christian on 26.10.14.
+//  Copyright (c) 2014 Gerloff, Christian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
